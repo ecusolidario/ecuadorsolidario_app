a5017ac33cef5e7700d2b2a3046740a0430f6986
Pelayo Maojo <pelayo.mao@gmail.com>
2016-05-26 22:18:40 +0100

    database fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525221235) do
+ActiveRecord::Schema.define(version: 20160526211655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,5 +74,5 @@ ActiveRecord::Schema.define(version: 20160525221235) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "casos", "users"
-  add_foreign_key "recursos", "casos"
+  add_foreign_key "recursos", "casos", on_delete: :cascade
 end
