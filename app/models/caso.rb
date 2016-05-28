2febe7792520ebfe81398240e83bfa54d7b0d94f
Pelayo Maojo <pelayo.mao@gmail.com>
2016-05-28 23:34:40 +0100

    imágenes no se suben paperclip...??

--- app/models/caso.rb
+++ app/models/caso.rb
@@ -1,15 +1,16 @@
 class Caso < ActiveRecord::Base
+  belongs_to :user
   has_many :imagens
+  has_many :recursos
+
+  accepts_nested_attributes_for :recursos, :imagens
+
   geocoded_by :ubicacion
   validates_presence_of :titulo, :problema, :ubicacion
   YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([\A#\&\?]*).*/i
   if :youtubevideo
     validates :youtubevideo, presence: false, format: YT_LINK_FORMAT
   end
-  belongs_to :user
-  has_many :recursos
-  accepts_nested_attributes_for :recursos
-  accepts_nested_attributes_for :imagens
   def to_param
   "#{id} #{titulo}".parameterize
   end
