7e3674c9001e8f8c8f54eff874056331052a5160
Pelayo Maojo <pelayo.mao@gmail.com>
2016-05-28 01:07:16 +0100

    en proceso, implementando imágenes de caso

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527233149) do
+ActiveRecord::Schema.define(version: 20160527234817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160527233149) do
     t.datetime "imagen_updated_at"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.integer  "caso_id"
   end
 
   create_table "recursos", force: :cascade do |t|
