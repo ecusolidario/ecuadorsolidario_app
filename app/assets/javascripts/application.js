6e26a5a1db8fa33f67bd2d67080c1dfa932d8220
Pelayo Maojo <pelayo.mao@gmail.com>
2016-05-30 00:50:25 +0100

    varios

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require jquery_nested_form
 //= require bootstrap-notify
---
+++ app/assets/javascripts/casos.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=504323752911178";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+});
