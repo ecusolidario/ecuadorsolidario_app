21c8994db5cc695653a43fd8e634d11ff7a7d765
Pelayo Maojo <pelayo.mao@gmail.com>
2016-05-31 01:12:28 +0100

    quitar show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :admin_only, :except => :show
+  before_action :admin_only
 
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
-    unless current_user.admin?
-      unless @user == current_user
-        redirect_to :back, :alert => "Access denied."
-      end
-    end
+    redirect_to root_path
   end
 
   def update
